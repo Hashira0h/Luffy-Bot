75158b6b97a3ff1323219760766ac76dcbab3f93
GOJO1! <139925219+Hashira0h@users.noreply.github.com>
2023-09-04 05:37:43 -0700

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['201206178781', 'ɢᴏᴊᴏ', true],
-  ['967780763155', 'BOT', true], 
+  ['967714075210', 'ɢᴏᴊᴏ', true],
+  ['967714075210', 'LUFFY BOT', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['201159250900','201206178781'] 
-global.prems = ['201206178781', '201159250900', '967780763155']
+global.mods = ['967714075210''] 
+global.prems = ['967714075210']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -47,9 +47,9 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = '𝐁𝐎𝐓'
+global.botname = 'LUFFY'
 global.premium = 'true'
-global.packname = '𝐁𝐎𝐓' 
+global.packname = 'LUFFY BOT' 
 global.author = 'صلي ؏ النبي' 
 global.igfg = 'Follow on Instagram\nhttps://instagram.com/mohamedmostafa4769?igshid=ZGUzMzM3NWJiOQ==\n' 
 global.dygp = 'https://chat.whatsapp.com/JcWLIylfbI5I0wazsJ7xEB'
@@ -59,7 +59,7 @@ global.fgpyp = 'https://youtube.com/@Mo_yt74'
 global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
 global.dbase = 'mongodb+srv://guru:guru@cluster0.qpggl6x.mongodb.net/?retryWrites=true&w=majority' //ADD YOUR MONGODB BY CHANGIMG IT
 
-global.wait = '*⌛ _جاري التحميل..._*\n*▰▰▰▱▱▱▱▱*'
+global.wait = '*⌛ جاري التحميل ▰▰▰▱▱▱▱▱*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✅'
